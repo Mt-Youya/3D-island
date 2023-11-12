60d93c3829fa9e334289f49eb3b08d3be066ccce
Mt-Youya <dd257248@163.com>
2023-11-12 22:48:12 +0800

    fix: 修复 图片问题

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,15 +1,13 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
+import meta from "./meta.png"
+import shopify from "./shopify.png"
+import starbucks from "./starbucks.png"
+import tesla from "./tesla.png"
+import logo from "./logo.svg"
 
 export {
-    hero,
     meta,
     shopify,
     starbucks,
     tesla,
-    logo
+    logo,
 }
